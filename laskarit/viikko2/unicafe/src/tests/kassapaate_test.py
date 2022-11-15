9da0b457bea86aed8b7045b8f1b2c6cf030793fb
renvik <51605816+renvik@users.noreply.github.com>
2022-11-15 20:14:51 +0200

    kassapaatteen testit

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,5 +1,15 @@
 import unittest
 from kassapaate import Kassapaate
 
-class TestMaksukortti(unittest.TestCase):
-    pass
+class Kassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassassa_rahaa = 100000
+        self.edulliset = 0
+        self.maukkaat = 0
+
+    def test_saldot_on_alussa_oikein(self):
+        self.assertEqual(str(self.kassassa_rahaa), "100000")
+        self.assertEqual(str(self.edulliset), "0")
+        self.assertEqual(str(self.maukkaat), "0")
+    
+        
