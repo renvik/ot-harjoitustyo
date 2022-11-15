daa9f6d39b88ce0db6d3d65cc53209c3e89cc1f4
renvik <51605816+renvik@users.noreply.github.com>
2022-11-15 22:06:47 +0200

    viimeinen palautus

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -11,5 +11,14 @@ class Kassapaate(unittest.TestCase):
         self.assertEqual(str(self.kassassa_rahaa), "100000")
         self.assertEqual(str(self.edulliset), "0")
         self.assertEqual(str(self.maukkaat), "0")
+
+    def kateisosto_toimii_edullisten_osalta(self):
+        self.assertEqual(str(self.kassassa_rahaa), self.kassassa_rahaa + 240)
+        #self.assertEqual(str(self.edulliset), self.edulliset += 1)       
     
-        
+    def kateisosto_toimii_maukkaiden_osalta(self):
+        self.assertEqual(str(self.kassassa_rahaa), self.kassassa_rahaa + 400)
+
+    #def riittava_maksu_kasvattaa_kassaa(self):
+
+
