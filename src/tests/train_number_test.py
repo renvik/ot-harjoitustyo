5cc1b085fce3c7cbfc347375e803aa3447e8b9b7
renvik <51605816+renvik@users.noreply.github.com>
2022-12-25 22:03:33 +0200

    final submit

--- src/tests/train_number_test.py
+++ src/tests/train_number_test.py
@@ -7,11 +7,3 @@ class TestTrainnumber(unittest.TestCase):
    
     def setUp(self):
         self.train_repository = TrainRepository()
-
-    # tests that the api response status code is 200 
-    def test_api_response(self):
-        train_data = requests.get(
-            f'https://rata.digitraffic.fi/api/v1/trains/2022-12-24/1')
-        response = print(train_data.status_code)
-        self.assertEqual(str(response), "200")
-
